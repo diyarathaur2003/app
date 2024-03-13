cd3bca28b8bda002e8eff491ed6017b8f42f37e0
diyarathaur2003 <diyarathaur312@gmail.com>
2024-03-13 13:56:44 +0530

    ne1

--- obj/Release/net8.0/app.AssemblyInfo.cs
+++ obj/Release/net8.0/app.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+822cc376c4178f5e898289d5916920b0c97c4c86")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
